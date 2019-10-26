1079047e3ee5c7d609c64b50be644882b82393b8
HollyStrauch <hollystrauch14@gmail.com>
2019-10-26 10:44:42 -0700

    Begin game set up

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
 //
 // Created by Holly Strauch on 10/25/2019.
 //
+#include "Player.h"
+#include "Property.h"
+
+
+
+Property** start_props(){
+
+    Property **start_props = new Property*;
+
+    for(int i = 0; i < 3; i++){
+        start_props[i + 0][i] = new House();
+        start_props[i + 1][i] = new Apt();
+        start_props[i + 2][i] = new Buisiness();
+    }
+
+    return start_props;
+}
+
+int main(){
+
+    //setup
+    Player* player1 = new Player("Holly");
+
+    Property** avail_props = start_props();
+
+
+
+    return 0;
+}
 
