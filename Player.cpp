23acaf3ad908344638c94311344238cd2cefb5f1
HollyStrauch <hollystrauch14@gmail.com>
2019-10-26 10:43:00 -0700

    Create Player Class

--- Player.cpp
+++ Player.cpp
@@ -2,4 +2,164 @@
 // Created by Holly Strauch on 10/25/2019.
 //
 
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
 #include "Player.h"
+
+using namespace std;
+
+Player::Player(string name){
+    this->name = name;
+    this->bank_account = 5000000.0;
+    this->num_prop = 0;
+    this->head = nullptr;
+    this->tail = nullptr;
+}
+
+Player::~Player(){
+
+    Node *temp = this->head;
+    for (int i = 0; i < this->num_prop; i++){
+
+        delete head->p;
+        head->p = NULL;
+
+        this->head = head->next;
+
+        delete temp;
+        temp = this->head;
+    }
+
+    head = nullptr;
+    tail = nullptr;
+}
+
+void Player::sell_property() {
+
+    cout << "SELL PROPERTIES: \n Listing all properties.  Properties must contain no tenants in order to sell." << endl;
+    this->print_prop();
+    int index = 0;
+
+    do {
+        index = user_input_prop(this->num_prop);
+    }while(!check_avail(index - 1));
+
+    Property* sell = get_prop(index - 1);
+    sale_price(sell->get_value());
+
+    del_at_index(index - 1);
+}
+
+void Player::print_prop() {
+
+    cout << "\t Type \t Value \t Location \t No. of Tenants " << endl;
+
+    Node *temp = this->head;
+    for (int i = 0; i < this->num_prop; i++){
+
+        cout << i + 1 << ". " << temp->p->get_type() << " " << temp->p->get_value() << " "
+             << temp->p->get_loc() << " " << temp->p->get_num_tenants() << " " << temp->p->get_mort() << endl;
+            temp = temp->next;
+    }
+
+    delete temp;
+
+}
+
+void Player::del_at_index(int index){
+
+    Node* temp = this->head;
+    for(int i = 0; i < index - 1; i++){
+        temp = temp->next;
+    }
+
+    Node* del = temp->next;
+    temp->next = temp->next->next;
+
+    this->num_prop--;
+
+    delete del;
+    delete temp;
+}
+
+Property* Player::get_prop(int index){
+    Node* temp = this->head;
+    for(int i = 0; i < index - 1; i++){
+        temp = temp->next;
+    }
+
+    return temp->p;
+}
+
+
+int Player::user_input_prop(int total){
+
+    cout << "Enter the number of the property you would like to select" << endl;
+
+    while (true){
+        string input;
+        getline(cin, input);
+
+        try{
+            int num = stoi(input);
+            if( num > 0 && num <= total){
+                return num;
+            }
+        }catch(invalid_argument const &e){
+            cout << "Invalid entry, please try again" << endl;
+        }
+    }
+
+}
+
+int Player::sale_price(double prop_value) {
+
+    cout << "Enter the amount you'd like to sell the property for";
+    double price = user_input_price();
+    int r = rand() % 3 + 1;
+
+    switch(r){
+        case 1: cout << "What a fantastic piece of property!! Your price as been accepted!" << endl;
+            this->bank_account += price;
+            break;
+        case 2: cout << "The buyer will pay the property value" << endl;
+            this->bank_account += prop_value;
+            break;
+        case 3: cout << "No one likes your property. You'll get 10% less than property value" << endl;
+            this->bank_account += prop_value * .9;
+    }
+}
+
+double Player::user_input_price(){
+
+    while (true){
+        string input;
+        getline(cin, input);
+
+        try{
+            double num = stod(input);
+            if( num > 0 ){
+                return num;
+            }
+        }catch(invalid_argument const &e){
+            cout << "Invalid entry, please try again" << endl;
+        }
+    }
+}
+
+bool Player::check_avail(int index){
+    Node* temp = this->head;
+
+    for(int i = 0; i < index; i++){
+        temp = temp->next;
+    }
+
+    if(temp->p->get_num_tenants() == 0){
+        cout << "Property is available to sell" << endl;
+        return true;
+    }else{
+        cout << "Property contains tenants and is unavailable to sell" << endl;
+        return false;
+    }
+}
--- Player.h
+++ Player.h
@@ -5,4 +5,33 @@
 #ifndef REAL_ESTATE_TYCOON_PLAYER_H
 #define REAL_ESTATE_TYCOON_PLAYER_H
 
+#include "Property.h"
+
+class Player{
+    double bank_account;
+    int num_prop;
+    //Property* props;
+    string name;
+
+    struct Node {
+        Property *p;
+        Node *next;
+    };
+    Node *head;
+    Node *tail;
+
+public:
+    Player(string name);
+    ~Player();
+    void sell_property();
+    void print_prop();
+    Property buy_prop(Property* prop);
+    void del_at_index(int index);
+    Property* get_prop(int index);
+    int user_input_prop(int total);
+    int sale_price(double prop_value);
+    bool check_avail(int index);
+    double user_input_price();
+};
+
 #endif //REAL_ESTATE_TYCOON_PLAYER_H
