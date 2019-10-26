e5395b00f60aae5f4d61877fdc49808d11ddadfb
HollyStrauch <hollystrauch14@gmail.com>
2019-10-26 10:44:13 -0700

    Tenant constructor and interface

--- Tenant.cpp
+++ Tenant.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Tenant.h"
+#include <stdlib.h>
+
+Tenant::Tenant(bool type){
+
+    this->type = type;
+    this->agree = rand() % 5 + 1;
+
+    if (type){
+        // citizen
+        this->budget = (rand() % 45 + 5) * 100;
+    }else{
+        // business
+        this->budget = (rand() % 8 + 2) * 1000;
+    }
+}
--- Tenant.h
+++ Tenant.h
@@ -5,4 +5,12 @@
 #ifndef REAL_ESTATE_TYCOON_TENANT_H
 #define REAL_ESTATE_TYCOON_TENANT_H
 
+class Tenant{
+    bool type;
+    int budget;
+    int agree;
+
+public:
+    Tenant(bool type);
+};
 #endif //REAL_ESTATE_TYCOON_TENANT_H
