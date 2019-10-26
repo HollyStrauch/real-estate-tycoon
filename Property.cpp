8f419972694b75303d2394c1ec4dfdc6181ade0a
HollyStrauch <hollystrauch14@gmail.com>
2019-10-26 10:43:39 -0700

    Begin Property Class

--- Property.cpp
+++ Property.cpp
@@ -3,3 +3,76 @@
 //
 
 #include "Property.h"
+
+void Property::random_event(int event) {
+
+    switch (event)
+    {
+        case 1: this->hurricane();
+            break;
+        case 2: this->tornado();
+            break;
+        case 3: this->earthquake();
+            break;
+        case 4: this->wildfire();
+            break;
+        case 5: this->crash();
+            break;
+        case 6: this->gent("temp");
+            break;
+    }
+}
+
+void Property::hurricane() {
+
+    if(!this->location.compare("SE")){
+        return;
+    }
+
+    this->value *= .5;
+}
+
+void Property::tornado() {
+
+    if(!this->location.compare("MW")){
+        return;
+    }
+
+    this->value *= .7;
+}
+
+void Property::earthquake() {
+
+    if(!this->location.compare("NW")){
+        return;
+    }
+
+    this->value *= .9;
+}
+
+void Property::wildfire(){
+    if(!this->location.compare("SW")){
+        return;
+    }
+
+    this->value *= .75;
+
+}
+
+void Property::crash(){
+    this->value *= .7;
+}
+
+void Property::gent(string loc){
+    if(!this->location.compare(loc)){
+        return;
+    }
+
+    this->value += this->value * .2 ;
+
+}
+
+string Property::get_type() const{
+    return type;
+}
+
--- Property.h
+++ Property.h
@@ -5,4 +5,44 @@
 #ifndef REAL_ESTATE_TYCOON_PROPERY_H
 #define REAL_ESTATE_TYCOON_PROPERTY_H
 
+#include <string>
+#include "Tenant.h"
+using namespace std;
+
+
+class Property{
+    double value;
+    double mortgage;
+    double dur_mortgage;
+    double propery_tax;
+    int num_tenants;
+    Tenant* tenants;
+    string location;
+    string type;
+
+public:
+    Property();
+    double get_value() const;
+    string get_loc() const;
+    int get_num_tenants() const;
+    Tenant* get_tenants();
+    double calc_mortgage();
+    Property gen_random(int event);
+    ~Property();
+    void weather(int event);
+    void hurricane();
+    void tornado();
+    void earthquake();
+    void wildfire();
+    void crash();
+    void gent(string loc);
+    void random_event(int event);
+    string get_type() const;
+    double get_mort() const;
+
+
+
+};
+
+
 #endif //REAL_ESTATE_TYCOON_PROPERY_H
