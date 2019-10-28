8517434cbcd1a0bb879c3698bd3c0e548dda384e
HollyStrauch <hollystrauch14@gmail.com>
2019-10-28 15:08:52 -0700

    create Game class

---
+++ Game.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Holly Strauch on 10/28/2019.
+//
+
+#include "Game.h"
+#include "Player.h"
+
+Game::Game(){
+    this->avail_a = new Apt[3];
+    this->avail_h = new House[3];
+    this->avail_b = new Business[3];
+}
+
+void Game::start_game() {
+    Player* player1 = new Player("Holly");
+
+}
---
+++ Game.h
@@ -0,0 +1,23 @@
+//
+// Created by Holly Strauch on 10/28/2019.
+//
+
+#ifndef REAL_ESTATE_TYCOON_GAME_H
+#define REAL_ESTATE_TYCOON_GAME_H
+
+#include "House.h"
+#include "Apt.h"
+#include "Business.h"
+
+class Game {
+    House* avail_h;
+    Apt* avail_a;
+    Business* avail_b;
+public:
+    Game();
+    void start_game();
+
+};
+
+
+#endif //REAL_ESTATE_TYCOON_GAME_H
