1bd143a2ebb9fd5985e6fda4e842ab15b51cd9ec
HollyStrauch <hollystrauch14@gmail.com>
2019-10-30 17:04:05 -0700

    fix tenant construction

--- House.cpp
+++ House.cpp
@@ -9,10 +9,11 @@ House::House(){
     this->mortgage = (rand() % 50 + 1) * 100;
     this->propery_tax = this->value * .015;
     this->dur_mortgage = this->value / this->mortgage;
+    this->rent = 0;
 
     this->type = "HOUSE";
     this->num_tenants = 1;
-    this->tenants = new Tenant*;
+    this->tenants = new Tenant(true);
     this->set_loc();
 
 }
