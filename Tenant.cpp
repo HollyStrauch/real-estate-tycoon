50ad75b9202b78190330123d2b565ac3e73c1409
HollyStrauch <hollystrauch14@gmail.com>
2019-10-30 17:06:24 -0700

    Create getters

--- Tenant.cpp
+++ Tenant.cpp
@@ -9,6 +9,7 @@ Tenant::Tenant(bool type){
 
     this->type = type;
     this->agree = rand() % 5 + 1;
+    this->exists = true;
 
     if (type){
         // citizen
@@ -17,4 +18,31 @@ Tenant::Tenant(bool type){
         // business
         this->budget = (rand() % 8 + 2) * 1000;
     }
+}
+
+Tenant::Tenant(){
+    this->type = false;
+    this->exists = false;
+    this->agree = 0;
+    this->budget = 0;
+}
+
+int Tenant::get_agree() const {
+    return this->agree;
+}
+
+int Tenant::get_budget() const {
+    return this->budget;
+}
+
+bool Tenant::get_type() const {
+    return this->type;
+}
+
+bool Tenant::get_exists() const {
+    return this->exists;
+}
+
+void Tenant::set_not_exist() {
+    this->exists = false;
 }
--- Tenant.h
+++ Tenant.h
@@ -7,10 +7,18 @@
 
 class Tenant{
     bool type;
+    bool exists;
     int budget;
     int agree;
 
+
 public:
     Tenant(bool type);
+    Tenant();
+    int get_agree() const;
+    int get_budget() const;
+    bool get_type() const;
+    bool get_exists() const;
+    void set_not_exist();
 };
 #endif //REAL_ESTATE_TYCOON_TENANT_H
